4c572df303dbb587ad87c60629a7fd61a1b0c58e
Piyush1817 <lakwanisuresh086@gmail.com>
2026-02-16 17:48:19 +0530

    clearing with encapsulation

--- src/main/java/com/piyush/springdemo/hello_world.java
+++ src/main/java/com/piyush/springdemo/hello_world.java
@@ -3,11 +3,15 @@ package com.piyush.springdemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class hello_world {
 
+    private String name = "Piyush";
+
     @GetMapping("/name")
     public String getName() {
-        return "My name is Piyush";
+        System.out.println("Hello World " + name);
+        return name;
     }
 }
