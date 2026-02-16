65012d96ba8310e9ae587dc813b409cc67fa1ad7
Piyush1817 <lakwanisuresh086@gmail.com>
2026-02-16 18:08:06 +0530

    clearing with abstraction

--- src/main/java/com/piyush/springdemo/hello_world.java
+++ src/main/java/com/piyush/springdemo/hello_world.java
@@ -3,15 +3,45 @@ package com.piyush.springdemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class hello_world {
-
-    private String name = "Piyush";
-
+    private String name;
     @GetMapping("/name")
     public String getName() {
-        System.out.println("Hello World " + name);
-        return name;
+        return name ;
     }
 }
+
+
+//  PaymentService.java
+// package com.piyush.springdemo;
+
+// public interface PaymentService {
+//     void pay(double amount);
+// }
+
+
+
+
+// UpiPayment.java
+// package com.piyush.springdemo;
+// public class UpiPayment implements PaymentService {
+//     @Override
+//     public void pay(double amount) {
+//         System.out.println("Paying " + amount + " using UPI");
+//     }
+// }
+
+
+// Main.java
+// package com.piyush.springdemo;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+
+//         PaymentService paymentService = new UpiPayment();
+//         paymentService.pay(500);
+
+//     }
+// }
